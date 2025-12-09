5d5a3bbc500689c3b8615db92b16b956a0d7ea5b
Gosa Girma <gosagirma441@gmail.com>
2025-12-09 13:39:28 +0300

    let's make some mess

--- movie-app/src/Components/Header.jsx/Header.jsx
+++ movie-app/src/Components/Header.jsx/Header.jsx
@@ -3,7 +3,8 @@ const Header=()=>{
     return(
         <div className="header-container">
             <input className='search-input' type="text" placeholder="Search Movies"/>
-            <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>    
+            <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
+            <button className="search-button">watch list</button>    
         </div>
     )
 }
