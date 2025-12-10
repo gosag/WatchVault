b45092f424e27c4988b266985f6c312b29e851b5
Gosa Girma <gosagirma441@gmail.com>
2025-12-10 17:43:46 +0300

    made the toggle buton to change the color when toggled

--- movie-app/src/Components/Header.jsx/Header.jsx
+++ movie-app/src/Components/Header.jsx/Header.jsx
@@ -1,14 +1,18 @@
 import './Header.css'
+import {useState} from 'react'
 const Header=({watchList})=>{
     function AddedTowatch(){
         console.log(watchList)
     }
+const [isToggled,setIsToggled]=useState(false)
     return(
         <div className="header-container">
             <input className='search-input' type="text" placeholder="Search Movies"/>
             <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
             <button className="search-button" onClick={AddedTowatch}>watch list</button>
-            <div className="toggle-theme"></div> 
+            <div className={`toggle-theme ${!isToggled?"not-toggled":"toggled"}`} onClick={()=>{setIsToggled(prevS=>!prevS)}}>
+               <div className={`toggle-circle`}></div> 
+            </div> 
         </div>
     )
 }
