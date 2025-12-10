f30c3c5ebbaefeca86be4d1594e4f85ab94f0bf2
Gosa Girma <gosagirma441@gmail.com>
2025-12-10 17:16:13 +0300

    adde toggle theme to the header

--- movie-app/src/Components/Header.jsx/Header.jsx
+++ movie-app/src/Components/Header.jsx/Header.jsx
@@ -7,7 +7,8 @@ const Header=({watchList})=>{
         <div className="header-container">
             <input className='search-input' type="text" placeholder="Search Movies"/>
             <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
-            <button className="search-button" onClick={AddedTowatch}>watch list</button>    
+            <button className="search-button" onClick={AddedTowatch}>watch list</button>
+            <div className="toggle-theme"></div> 
         </div>
     )
 }
