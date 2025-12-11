4b78308638b7c18051bb99668766b487a7471b7d
Gosa Girma <gosagirma441@gmail.com>
2025-12-11 23:19:52 +0300

    started handling the search bar

--- movie-app/src/Components/Header.jsx/Header.jsx
+++ movie-app/src/Components/Header.jsx/Header.jsx
@@ -1,15 +1,25 @@
 import './Header.css'
 import {useState} from 'react'
-const Header=({watchList,isToggled,setIsToggled})=>{
-    function AddedTowatch(){
-        console.log(watchList)
-    }
 
+const Header=({watchList,isToggled,setIsToggled})=>{
+function AddedTowatch(){
+    console.log(watchList)
+}
+const [searchValue,setSearchValue]=useState('')
+function onChangeHandler(event){
+    setSearchValue(event.target.value)
+}
     return(
         <div className="header-container">
-            <input className='search-input' type="text" placeholder="Search Movies"/>
+            <div className='inner-header-container'>
+            <div>
+            <input className='search-input' value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}} type="text"  placeholder="Search Movies"/>
             <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
-            <button className="search-button" onClick={AddedTowatch}>watch list</button>
+            </div> 
+            <button className="search-button watchlist" onClick={AddedTowatch}>
+                <i class="fa-solid fa-bookmark"></i>
+            </button>
+            </div>
             <div className={`toggle-theme ${!isToggled?"not-toggled":"toggled"}`} onClick={()=>{setIsToggled(prevS=>!prevS)}}>
                <div className={`toggle-circle ${isToggled?"on-state":"off-state"}`}></div> 
             </div> 
