52d70eff874cf3a2af615eebb876f1c29857a948
Gosa Girma <gosagirma441@gmail.com>
2025-12-11 23:33:40 +0300

    made the watch list button to stay at the side

--- movie-app/src/Components/Header.jsx/Header.jsx
+++ movie-app/src/Components/Header.jsx/Header.jsx
@@ -12,14 +12,12 @@ function onChangeHandler(event){
     return(
         <div className="header-container">
             <div className='inner-header-container'>
-            <div>
             <input className='search-input' value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}} type="text"  placeholder="Search Movies"/>
             <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
-            </div> 
+            </div>
             <button className="search-button watchlist" onClick={AddedTowatch}>
                 <i class="fa-solid fa-bookmark"></i>
             </button>
-            </div>
             <div className={`toggle-theme ${!isToggled?"not-toggled":"toggled"}`} onClick={()=>{setIsToggled(prevS=>!prevS)}}>
                <div className={`toggle-circle ${isToggled?"on-state":"off-state"}`}></div> 
             </div> 
