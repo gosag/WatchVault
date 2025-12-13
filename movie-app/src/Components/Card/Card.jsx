568e61fce4217d8ddc7ff0582251dc7c9b3612cc
Gosa Girma <gosagirma441@gmail.com>
2025-12-13 15:41:16 +0300

    updated the card component isFavourite state

--- movie-app/src/Components/Card/Card.jsx
+++ movie-app/src/Components/Card/Card.jsx
@@ -1,108 +1,84 @@
-import './Card.css'
-import {useState,useEffect} from 'react'
-import { Link } from 'react-router-dom'
-const Card=({tryMovie,watchList,setWatchList,isToggled})=>{
+import "./Card.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const [ratingValue,setRatingValue]=useState('rating-0.png')
+const Card = ({ tryMovie, watchList, setWatchList, isToggled }) => {
+  const [ratingValue, setRatingValue] = useState("rating-0.png");
 
-function ChangeVoteAverage() {
-    const Vote = tryMovie.vote_average;
+  useEffect(() => {
+    const vote = tryMovie.vote_average;
+    if (vote < 1) setRatingValue("rating-0.png");
+    else if (vote < 2) setRatingValue("rating-05.png");
+    else if (vote < 3) setRatingValue("rating-10.png");
+    else if (vote < 4) setRatingValue("rating-15.png");
+    else if (vote < 5) setRatingValue("rating-20.png");
+    else if (vote < 6) setRatingValue("rating-25.png");
+    else if (vote < 7) setRatingValue("rating-30.png");
+    else if (vote < 8) setRatingValue("rating-35.png");
+    else if (vote < 9) setRatingValue("rating-40.png");
+    else if (vote < 10) setRatingValue("rating-45.png");
+    else setRatingValue("rating-50.png");
+  }, [tryMovie.vote_average]);
+  const isFavorite = watchList.some(
+    movie => movie.id === tryMovie.id
+  );
 
-    if (Vote < 1) {
-        setRatingValue('rating-0.png');
+  function toggleWatchList() {
+    if (isFavorite) {
+      setWatchList(prev =>
+        prev.filter(movie => movie.id !== tryMovie.id)
+      );
+    } else {
+      setWatchList(prev => [...prev, tryMovie]);
     }
-    else if (Vote < 2) {
-        setRatingValue('rating-05.png');
-    }
-    else if (Vote < 3) {
-        setRatingValue('rating-10.png');
-    }
-    else if (Vote < 4) {
-        setRatingValue('rating-15.png');
-    }
-    else if (Vote < 5) {
-        setRatingValue('rating-20.png');
-    }
-    else if (Vote < 6) {
-        setRatingValue('rating-25.png');
-    }
-    else if (Vote < 7) {
-        setRatingValue('rating-30.png');
-    }
-    else if (Vote < 8) {
-        setRatingValue('rating-35.png');
-    }
-    else if (Vote < 9) {
-        setRatingValue('rating-40.png');
-    }
-    else if (Vote < 10) {
-        setRatingValue('rating-45.png');
-    }
-    else {
-        setRatingValue('rating-50.png');
-    }
-}
-useEffect(()=>{
-    ChangeVoteAverage();
-},[tryMovie])
-  const [tempWatch,setTempwatch]=useState(null);
-const matchFound=watchList.find((item)=>item.id===tryMovie.id)
-function AddToWatchList(){
-    setTempwatch(tryMovie);
-    setIsFavourite(preS=>!preS)
-}
-useEffect(()=>{
-    if (!tempWatch) return;
-    try{
-    if(!matchFound){
-        setWatchList((prevState)=>[...prevState,tempWatch]);
-    }
-     
-    }catch(error){
-        console.log('caught error:',error);
-    }
-    return ()=>{console.log("cleanup")};
-},[tempWatch])
-function removeFromWatchList(){
-    if(watchList.length>0 && matchFound){
-        const filterdMovies=watchList.filter((movie)=>(movie.id!==tryMovie.id))
-        setWatchList(filterdMovies)
-        setIsFavourite(preS=>!preS)
-    }
-}
-const aMatchFound=watchList.some((item)=>item.id===tryMovie.id)
-useEffect(()=>{
- if(aMatchFound){
-    setIsFavourite(true)
- }
-},[watchList])
-const [isFavorite,setIsFavourite]=useState(false);
-    return(
-        <div className={`card ${isToggled?"dim-light-card":""}`}>
-                <div className='inner-card-container'>
-                  <div className='image-wrap'>
-                    {<img className='movie-poster blur' src={`https://image.tmdb.org/t/p/w500${tryMovie.poster_path}`} alt={tryMovie.title} />}
-                    <img className='movie-poster main' src={`https://image.tmdb.org/t/p/w500${tryMovie.poster_path}`} alt={tryMovie.title} />
-                  </div>
-                  <div className={`movie-title ${!isToggled?"black-title":""}`}>
-                             {tryMovie.title}
-                  </div>
-                  <div className='movie-info'>
-                        
-                        
-                     <div className="vote-average"> <img className='star-image' src={`../../../ratings/${ratingValue}`}/></div>
-                  
-                  </div>
-                  <Link to="/Details" state={{tryMovie:tryMovie,isToggled:isToggled}}>
-                         <button className="full-details">Details</button>
-                  </Link>
-                  <div className={`fav-container ${isFavorite?"is-favourite":''}`} onClick={!isFavorite?AddToWatchList:removeFromWatchList} >
-                     <i className="fa-solid fa-heart"></i>
-                  </div>
-                  
-                </div>
-                
+  }
+
+  return (
+    <div className={`card ${isToggled ? "dim-light-card" : ""}`}>
+      <div className="inner-card-container">
+        <div className="image-wrap">
+          <img
+            className="movie-poster blur"
+            src={`https://image.tmdb.org/t/p/w500${tryMovie.poster_path}`}
+            alt={tryMovie.title}
+          />
+          <img
+            className="movie-poster main"
+            src={`https://image.tmdb.org/t/p/w500${tryMovie.poster_path}`}
+            alt={tryMovie.title}
+          />
         </div>
-    )
-}
+
+        <div className={`movie-title ${!isToggled ? "black-title" : ""}`}>
+          {tryMovie.title}
+        </div>
+
+        <div className="movie-info">
+          <div className="vote-average">
+            <img
+              className="star-image"
+              src={`../../../ratings/${ratingValue}`}
+              alt="rating"
+            />
+          </div>
+        </div>
+
+        <Link
+          to="/Details"
+          state={{ tryMovie, isToggled }}
+        >
+          <button className="full-details">Details</button>
+        </Link>
+
+        <div
+          className={`fav-container ${isFavorite ? "is-favourite" : ""}`}
+          onClick={toggleWatchList}
+        >
+          <i className="fa-solid fa-heart"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Card;
