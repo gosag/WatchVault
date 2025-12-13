120b31c7ab448f31013eea42f8cbc46e1edfeb24
Gosa Girma <gosagirma441@gmail.com>
2025-12-13 16:44:56 +0300

    moved the details and the watchlist into their own folders

--- movie-app/src/main.jsx
+++ movie-app/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import WatchList from './Components/watchList.jsx'
-import Details from './Components/Details.jsx'
+import WatchList from './Components/watchList/watchList.jsx'
+import Details from './Components/Details/Details.jsx'
 import { createBrowserRouter,RouterProvider} from 'react-router-dom'
 const router=createBrowserRouter([
   {path:"/",element:<App/>},
