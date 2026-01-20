9347d2b59b7301ca02dfce2f7a37a7979fe66d59
Gosa Girma <gosagirma441@gmail.com>
2026-01-20 20:34:38 +0300

    learn react controlled form

--- react-playground/src/App.tsx
+++ react-playground/src/App.tsx
@@ -1,15 +1,43 @@
-
+import { useState } from 'react'
 import './App.css'
 function App() {
- 
-
+const [form, setForm] = useState({
+  firstName:'',
+  lastName:'',
+  email:''})
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <form onSubmit={
+          (e) => {e.preventDefault(),
+          console.log(form)
+          }}>
+          <input
+            type='text'
+            placeholder='First Name'
+            value={form.firstName}
+            onChange={(e) => setForm({...form, firstName: e.target.value})}
+          />
+          <br/>
+          <input
+            type='text'
+            placeholder='Last Name'
+            value={form.lastName}
+            onChange={(e) => setForm({...form, lastName: e.target.value})}
+          />
+          <br/>
+          <input
+            type='email'
+            placeholder='Email'
+            value={form.email}
+            onChange={(e) => setForm({...form, email: e.target.value})}
+          />
+          <br/>
+          <button type='submit'>Submit</button>
+        </form>
+        <h1>Hey B*tch, Guess who is back?</h1>
       </div>
     </>
   )
 }
-
 export default App
